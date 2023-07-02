d1f0b71bddc4819407ed680cb5bce3d2cc60c9b6
FantasticSukhi <122316561+FantasticSukhi@users.noreply.github.com>
2023-07-02 22:56:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather on Telegram
-BOT_TOKEN = "5303637649:AAEZvD4CtFO7JcGaoz1D75pVsNtNtgQKjUY"
+BOT_TOKEN = ""
 
 # Project Name of your Project
 PROJECT_NAME = "GitGram"
 
 # If you forked the Repository, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/STD-DEEPANSHU/GitGram"
+GIT_REPO_URL = "https://github.com/FantasticSukhi/GitGram"
